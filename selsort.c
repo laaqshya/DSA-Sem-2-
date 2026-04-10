1670f2c77efc04c75dd33e1b1dcd8a50f7c45d52
Laaqshya <laaqshya@gmail.com>
2026-04-10 12:01:38 +0530

    Implement selection sort in selsort.c

---
+++ selsort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int a[50], n, i, j, min, t;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Enter elements of array:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    // Selection Sort
+    for(i=0;i<n;i++) {
+        min = i;
+
+        for(j = i + 1; j < n; j++) {
+            if(a[j] < a[min]) {
+                min = j;
+            }
+        }
+        t= a[i];
+        a[i] = a[min];
+        a[min] = t;
+    }
+    printf("Sorted array:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
