a3c51c2755c60cee7064055c2cd8a23704951ac6
Laaqshya <laaqshya@gmail.com>
2026-04-13 13:35:02 +0530

    Update and rename quickset.c to quicksort.c

---
+++ quicksort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int partition(int a[], int l, int h) {
+    int p=a[l];
+    int i=l+1,j=h,t;
+    while (1) {
+        while (i<=h && a[i]<=p) 
+		i++;
+        while (a[j]>p) 
+		j--;
+        if (i < j) {
+            t=a[i];
+            a[i]=a[j];
+            arr[j]=t;
+        } 
+		else
+		break;
+    }
+
+    t=a[l];
+    a[l]=a[j];
+    a[j]=t;
+    return j;
+}
+
+void quicksort(int a[], int l, int h) {
+    if (l<h) {
+        int j = partition(a,l,h);
+        quicksort(a,l,j-1);
+        quicksort(a,j+1,h);
+    }
+}
+
+int main() {
+    int a[10],n,i;
+    printf("enter n:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    quicksort(a,0,n - 1);
+    for (i= 0;i<n; i++)
+        printf("%d ",a[i]);
+    return 0;
+}
