3cee77329d39eb8c75a23b5938172b550588ed1d
Laaqshya <laaqshya@gmail.com>
2026-06-07 18:13:26 +0530

    Update lprobe.c

--- lprobe.c
+++ lprobe.c
@@ -1,64 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 10
+#define MAX_LOAD_FACTOR 0.7
 
-int hashTable[SIZE];
+int *hashTable;
+int SIZE = 10;
+int count = 0;
 
-// Initialize hash table
 void initialize() {
-    for(int i = 0; i < SIZE; i++) {
+    hashTable = (int *)malloc(SIZE * sizeof(int));
+
+    for (int i = 0; i < SIZE; i++) {
         hashTable[i] = -1;
     }
 }
 
-// Hash function
 int hashFunction(int key) {
     return key % SIZE;
 }
 
-// Insert function
+float loadFactor() {
+    return (float)count / SIZE;
+}
+
+void insertValue(int key) {
+    int index = hashFunction(key);
+
+    while (hashTable[index] != -1) {
+        index = (index + 1) % SIZE;
+    }
+
+    hashTable[index] = key;
+    count++;
+}
+
+void rehash() {
+    int oldSize = SIZE;
+    int *oldTable = hashTable;
+
+    SIZE = SIZE * 2;
+
+    hashTable = (int *)malloc(SIZE * sizeof(int));
+
+    for (int i = 0; i < SIZE; i++) {
+        hashTable[i] = -1;
+    }
+
+    count = 0;
+
+    for (int i = 0; i < oldSize; i++) {
+        if (oldTable[i] != -1) {
+            insertValue(oldTable[i]);
+        }
+    }
+
+    free(oldTable);
+
+    printf("Hash table resized to %d and rehashed successfully.\n", SIZE);
+}
+
 void insert() {
-    int n, key, index, i;
+    int n, key;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    for(i = 0; i < n; i++) {
-
+    for (int i = 0; i < n; i++) {
         printf("Enter value %d: ", i + 1);
         scanf("%d", &key);
 
-        index = hashFunction(key);
-
-        // Linear probing
-        while(hashTable[index] != -1) {
-            index = (index + 1) % SIZE;
+        if ((float)(count + 1) / SIZE > MAX_LOAD_FACTOR) {
+            rehash();
         }
 
-        hashTable[index] = key;
-    }
+        insertValue(key);
 
-    printf("Elements inserted successfully.\n");
+        printf("Inserted %d\n", key);
+        printf("Current load factor = %.2f\n", loadFactor());
+    }
 }
 
-// Display function
 void display() {
-
     printf("\nHash Table:\n");
 
-    for(int i = 0; i < SIZE; i++) {
-
-        if(hashTable[i] == -1)
+    for (int i = 0; i < SIZE; i++) {
+        if (hashTable[i] == -1)
             printf("Index %d : EMPTY\n", i);
         else
             printf("Index %d : %d\n", i, hashTable[i]);
     }
+
+    printf("Table size = %d\n", SIZE);
+    printf("Number of elements = %d\n", count);
+    printf("Load factor = %.2f\n", loadFactor());
 }
 
-// Search function
 void search() {
-
     int key, index, startIndex;
 
     printf("Enter value to search: ");
@@ -67,25 +104,22 @@ void search() {
     index = hashFunction(key);
     startIndex = index;
 
-    while(hashTable[index] != -1) {
-
-        if(hashTable[index] == key) {
+    while (hashTable[index] != -1) {
+        if (hashTable[index] == key) {
             printf("Value found at index %d\n", index);
             return;
         }
 
         index = (index + 1) % SIZE;
 
-        if(index == startIndex)
+        if (index == startIndex)
             break;
     }
 
     printf("Value not found.\n");
 }
 
-// Delete function
 void deleteKey() {
-
     int key, index, startIndex;
 
     printf("Enter value to delete: ");
@@ -94,46 +128,43 @@ void deleteKey() {
     index = hashFunction(key);
     startIndex = index;
 
-    while(hashTable[index] != -1) {
-
-        if(hashTable[index] == key) {
-
+    while (hashTable[index] != -1) {
+        if (hashTable[index] == key) {
             hashTable[index] = -1;
+            count--;
 
             printf("Value deleted successfully.\n");
+            printf("Current load factor = %.2f\n", loadFactor());
             return;
         }
 
         index = (index + 1) % SIZE;
 
-        if(index == startIndex)
+        if (index == startIndex)
             break;
     }
 
     printf("Value not found.\n");
 }
 
-// Main function
 int main() {
-
     int choice;
 
     initialize();
 
-    while(1) {
-
+    while (1) {
         printf("\n--- Linear Probing Menu ---\n");
         printf("1. Insert\n");
         printf("2. Display\n");
         printf("3. Search\n");
         printf("4. Delete\n");
-        printf("5. Exit\n");
+        printf("5. Load Factor\n");
+        printf("6. Exit\n");
 
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch(choice) {
-
+        switch (choice) {
             case 1:
                 insert();
                 break;
@@ -151,6 +182,11 @@ int main() {
                 break;
 
             case 5:
+                printf("Current load factor = %.2f\n", loadFactor());
+                break;
+
+            case 6:
+                free(hashTable);
                 exit(0);
 
             default:
