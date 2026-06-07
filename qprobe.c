4fba1e57984dbfd8722690c531d173fc66900270
Laaqshya <laaqshya@gmail.com>
2026-06-07 18:24:22 +0530

    Refactor hash table implementation with dynamic sizing

--- qprobe.c
+++ qprobe.c
@@ -1,47 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 10
-#define EMPTY -1
-#define DELETED -2
+#define MAX_LOAD_FACTOR 0.7
 
-int hashTable[SIZE];
+int *hashTable;
+int SIZE = 10;
+int count = 0;
 
 void initialize() {
-    for(int i = 0; i < SIZE; i++) {
-        hashTable[i] = EMPTY;
-    }
+    hashTable = (int *)malloc(SIZE * sizeof(int));
+
+    for (int i = 0; i < SIZE; i++)
+        hashTable[i] = -1;
 }
 
 int hashFunction(int key) {
     return key % SIZE;
 }
 
+float loadFactor() {
+    return (float)count / SIZE;
+}
+
+void insertValue(int key) {
+    int index = hashFunction(key);
+    int newIndex;
+
+    for (int i = 0; i < SIZE; i++) {
+        newIndex = (index + i * i) % SIZE;
+
+        if (hashTable[newIndex] == -1) {
+            hashTable[newIndex] = key;
+            count++;
+            return;
+        }
+    }
+
+    printf("Could not insert %d\n", key);
+}
+
+void rehash() {
+    int oldSize = SIZE;
+    int *oldTable = hashTable;
+
+    SIZE = SIZE * 2;
+
+    hashTable = (int *)malloc(SIZE * sizeof(int));
+
+    for (int i = 0; i < SIZE; i++)
+        hashTable[i] = -1;
+
+    count = 0;
+
+    for (int i = 0; i < oldSize; i++) {
+        if (oldTable[i] != -1)
+            insertValue(oldTable[i]);
+    }
+
+    free(oldTable);
+
+    printf("Table size doubled to %d and rehashed.\n", SIZE);
+}
+
 void insert() {
-    int n, key, index, newIndex;
+    int n, key;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    for(int j = 0; j < n; j++) {
-        printf("Enter value %d: ", j + 1);
+    for (int i = 0; i < n; i++) {
+        printf("Enter value %d: ", i + 1);
         scanf("%d", &key);
 
-        index = hashFunction(key);
-
-        for(int i = 0; i < SIZE; i++) {
-            newIndex = (index + i * i) % SIZE;
-
-            if(hashTable[newIndex] == EMPTY || hashTable[newIndex] == DELETED) {
-                hashTable[newIndex] = key;
-                printf("%d inserted at index %d\n", key, newIndex);
-                break;
-            }
-
-            if(i == SIZE - 1) {
-                printf("Hash table is full. Cannot insert %d\n", key);
-            }
+        if ((float)(count + 1) / SIZE > MAX_LOAD_FACTOR) {
+            rehash();
         }
+
+        insertValue(key);
+
+        printf("Current load factor = %.2f\n", loadFactor());
     }
 }
 
@@ -53,42 +90,16 @@ void search() {
 
     index = hashFunction(key);
 
-    for(int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE; i++) {
         newIndex = (index + i * i) % SIZE;
 
-        if(hashTable[newIndex] == key) {
+        if (hashTable[newIndex] == key) {
             printf("Value found at index %d\n", newIndex);
             return;
         }
 
-        if(hashTable[newIndex] == EMPTY) {
+        if (hashTable[newIndex] == -1)
             break;
-        }
-    }
-
-    printf("Value not found.\n");
-}
-
-void deleteKey() {
-    int key, index, newIndex;
-
-    printf("Enter value to delete: ");
-    scanf("%d", &key);
-
-    index = hashFunction(key);
-
-    for(int i = 0; i < SIZE; i++) {
-        newIndex = (index + i * i) % SIZE;
-
-        if(hashTable[newIndex] == key) {
-            hashTable[newIndex] = DELETED;
-            printf("Value deleted successfully.\n");
-            return;
-        }
-
-        if(hashTable[newIndex] == EMPTY) {
-            break;
-        }
     }
 
     printf("Value not found.\n");
@@ -97,14 +108,16 @@ void deleteKey() {
 void display() {
     printf("\nHash Table:\n");
 
-    for(int i = 0; i < SIZE; i++) {
-        if(hashTable[i] == EMPTY)
+    for (int i = 0; i < SIZE; i++) {
+        if (hashTable[i] == -1)
             printf("Index %d : EMPTY\n", i);
-        else if(hashTable[i] == DELETED)
-            printf("Index %d : DELETED\n", i);
         else
             printf("Index %d : %d\n", i, hashTable[i]);
     }
+
+    printf("Table size = %d\n", SIZE);
+    printf("Number of elements = %d\n", count);
+    printf("Load factor = %.2f\n", loadFactor());
 }
 
 int main() {
@@ -112,39 +125,24 @@ int main() {
 
     initialize();
 
-    while(1) {
+    while (1) {
         printf("\n--- Quadratic Probing Menu ---\n");
         printf("1. Insert\n");
-        printf("2. Display\n");
-        printf("3. Search\n");
-        printf("4. Delete\n");
+        printf("2. Search\n");
+        printf("3. Display\n");
+        printf("4. Load Factor\n");
         printf("5. Exit\n");
 
-        printf("Enter your choice: ");
+        printf("Enter choice: ");
         scanf("%d", &choice);
 
-        switch(choice) {
-            case 1:
-                insert();
-                break;
-
-            case 2:
-                display();
-                break;
-
-            case 3:
-                search();
-                break;
-
-            case 4:
-                deleteKey();
-                break;
-
-            case 5:
-                exit(0);
-
-            default:
-                printf("Invalid choice.\n");
+        switch (choice) {
+            case 1: insert(); break;
+            case 2: search(); break;
+            case 3: display(); break;
+            case 4: printf("Current load factor = %.2f\n", loadFactor()); break;
+            case 5: free(hashTable); exit(0);
+            default: printf("Invalid choice\n");
         }
     }
 
